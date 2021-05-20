f8fe689ee1519d9bf717fd8b5b5908e05d3f2610
a1xg <64093496+a1xg@users.noreply.github.com>
2021-05-20 14:41:15 +0300

    backup 20.05.2021

--- safetyscan/apps/search/text_ocr.py
+++ safetyscan/apps/search/text_ocr.py
@@ -3,7 +3,6 @@ import cv2
 import re
 import pytesseract
 import pycountry
-import time
 from langdetect import detect, DetectorFactory
 # Tesseract location if environment variable is not working correctly
 # Tesseract version 5.0.0-alpha.20200328
@@ -17,24 +16,25 @@ pytesseract.pytesseract.tesseract_cmd = 'D:/Program/Tesseract-OCR/tesseract.exe'
 class ImageOCR:
     def __init__(self, img):
         self.img = img
-        self.text = [] # выходной список словарей с распознанным текстом в формате {'lang':'text'}
-        self.boxes = []
+        self.text = [] # output list of dictionaries with recognized text in the format {'lang':'text'}
+        self._boxes = []
 
-    def decodeImage(self):
-        '''Декодирует изображение из формата base64 в numpy массив'''
+    def decodeImage(self) -> np.ndarray:
+        '''Decode base64 image to numpy format'''
         self.img = cv2.imdecode(np.fromstring(self.img, np.uint8), cv2.IMREAD_UNCHANGED)
 
-    def _imagePreprocessing(self):
+    def _imagePreprocessing(self) -> np.ndarray:
+        '''Flattening the image histogram'''
         grayscale = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize= (8,8))
         normalised_img = clahe.apply(grayscale)
         self.img = normalised_img
 
-    def _resizeImage(self, image, max_resolution:int):
-        """ Метод изменяет размер изображения до максимально допустимого
-        с сохранением исходных пропорций независимо от вертикальной или
-        горизонтальной ориентации, например если максимальное измерение
-        задано, как 1000 пикселей, то могут быть получены 1000х800 или 800х1000
+    def _resizeImage(self, image, max_resolution:int) -> np.ndarray:
+        """ The method resizes the image to the maximum allowed
+        maintaining the original proportions regardless of vertical or
+        horizontal orientation, for example if the maximum dimension
+        set as 1000 pixels, then 1000x800 or 800x1000 can be obtained
         """
         max_dim = max(image.shape[0:2])
         scale_coef = max_dim / max_resolution
@@ -42,17 +42,15 @@ class ImageOCR:
         img_scaled = cv2.resize(image, new_dim, interpolation=cv2.INTER_CUBIC)
         return img_scaled
 
-    def _measureStrings(self):
-        '''
-        Метод подсчета текстовых строк на изображении
+    def _measureStrings(self) -> tuple:
+        '''Method of counting text lines in an image and measuring the average font height
         :param image:
         :return:
         '''
-        # в среднем время работы метода 0.17 сек
         num_lines = []
         font_size = []
-        # срезы изображения по оси X относительно ширины изображения, для повышения
-        # точности подсчета у изображений с искаженной перспективой.
+        # slices of the image along the X axis relative to the width of the image, to enhance
+        # counting accuracy for images with distorted perspective.
         slices = ((0.35, 0.45), (0.5, 0.6), (0.65, 0.75))
         height, width = self.img.shape[0:2]
         for slice in slices:
@@ -67,14 +65,14 @@ class ImageOCR:
             if len(lines) > 0:
                 font_size.extend([lines[i+1] - lines[i] for i in range(len(lines) - 1)])
                 num_lines.append(len(lines))
-        # Подчситываем среднюю высоту строки в пикселях
+        # Calculate the average line height in pixels
         mean_font_size = int(np.array(font_size).mean()) if len(font_size) > 1 else 0
-        # Подсчитываем среднее количество строк, хотя возможно эта метрика не пригодится
+        # Calculate the average number of text lines
         mean_num_lines = int(np.array(num_lines).mean()) if len(num_lines) > 1 else 0
 
         return (mean_font_size, mean_num_lines)
 
-    def _getTextMask(self, image, font_size:int, num_lines:int):
+    def _getTextMask(self, image, font_size:int, num_lines:int) -> np.ndarray:
         """
         The method searches the image for a text block and creates
         mask to use it to find outlines bounding the text and crop the excess part of the image.
@@ -82,13 +80,13 @@ class ImageOCR:
         rect_kernel_size = (100, 5)  # (W, H) default values 100, 5
         sq_kernel_size = (100, 5)  # (W, H) default values 100, 5
         if bool(font_size) == True:
-            # вычислем коэффициент разреженности текста на изображении
+            # calculate the coefficient of sparseness of the text in the image for optimal filter settings
             h_text_coef = font_size*num_lines/image.shape[0]
             rect_kernel_size = (int(font_size*0.6), int(font_size*0.4/h_text_coef))
             sq_kernel_size = (int(font_size*0.5), int(font_size*0.4/h_text_coef))
         elif bool(font_size) == False:
             None
-        # применяем фильтры изображений
+        # Apply filters to the image
         imgBlur = cv2.GaussianBlur(image, (3, 3), 0) # 3Х3
         rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, rect_kernel_size)
         sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, sq_kernel_size)  
@@ -101,8 +99,7 @@ class ImageOCR:
         threshold1 = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         threshold2 = cv2.morphologyEx(threshold1, cv2.MORPH_CLOSE, sqKernel)
         text_mask = cv2.erode(threshold2, None, iterations=4)
-        #cv2.imshow('test', text_mask)
-        #cv2.waitKey(0)
+
         # remove the edge of the image, so that in the future there
         # would be no outlines recognition outside the image
         border = int(image.shape[1] * 0.05)
@@ -111,73 +108,73 @@ class ImageOCR:
 
         return text_mask
 
-    def _getBinaryImages(self, image, font_size, new_font_size:int):
+    def _getBinaryImages(self, image, font_size, new_font_size:int) -> list:
         '''The method crops the text area of interest in the image, brings the 
-        resolution of the cropped area to the new standard maximum resolution 
-        (optimized for 1024 pixels), since the effectiveness of methods for 
-        increasing the quality of text differs greatly for images with different
-         resolutions
+        resolution of the cropped area to the new standard maximum resolution.
         '''
         binary_images = []
-        line_num_threshold = 2 # пороговое значение количества строк на изображении, все, что меньше будет удалено
-        # Если список ограничивающих рамок пуст, то ограничивающей рамкой будет все изображение
-        self.boxes = [[0, 0, image.shape[1], image.shape[0]]] if len(self.boxes) <= 0 else self.boxes
-        for box in self.boxes:
+        # Set the threshold for the number of lines in the image, images with fewer lines will be deleted
+        line_num_threshold = 2
+        # If the list of bounding boxes is empty, then the entire image will be the bounding box
+        self._boxes = [[0, 0, image.shape[1], image.shape[0]]] if len(self._boxes) <= 0 else self._boxes
+        # Cut out blocks with text from the original image using bounding boxes
+        for box in self._boxes:
             (x, y, w, h) = box
             cropped_img = image[y:y + h, x:x + w]
-            num_lines = int(cropped_img.shape[0]/font_size) # считаем количество строк на изображении
-            # Если количество строк больше порогового значения, то обрабатываем изображение, иначе пропускаем
+            num_lines = int(cropped_img.shape[0]/font_size) # count the number of lines in the image
+            # If the number of lines is more than the threshold value, then we process the image, otherwise we skip
             if num_lines > line_num_threshold:
-                #print('NUM LINES:', num_lines, '\nFONT SIZE:', font_size)
-                # размываем изображение
+                # blur the image
                 blur_size = int(np.ceil(font_size*0.2))
                 blur_size = blur_size if blur_size % 2 != 0 else blur_size + 1
-                gaussian_blured = cv2.GaussianBlur(cropped_img, (blur_size, blur_size), 10) # default 9,9
-                # улучшаем резкость изображения
-                test_sharpened = cv2.addWeighted(cropped_img, 1.5, gaussian_blured, -0.9, 0) # default 1.8, -1.5
-                # размываем изображение, что бы на следующем шаге минимизировать шум на изображении
+                gaussian_blured = cv2.GaussianBlur(cropped_img, (blur_size, blur_size), 10)
+                # sharpen the image
+                test_sharpened = cv2.addWeighted(cropped_img, 1.5, gaussian_blured, -0.9, 0)
+                # blur the image in order to minimize noise in the image in the next step
                 median_size = int(np.ceil(font_size*0.02))
                 median_size = median_size if median_size % 2 != 0 else median_size + 1
                 medianblur = cv2.medianBlur(test_sharpened, median_size) # default 3
-                # бинаризируем
+                # image binarization
                 test_thresh = cv2.threshold(medianblur, 0, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)[1]
-                # Считаем процентное соотношение черных пикселей отноcительно всего количества пикселей
+                # We calculate the percentage of black pixels relative to the total number of pixels
                 percent_zeros = int(cv2.countNonZero(test_thresh)*100/(test_thresh.shape[1]*test_thresh.shape[0]))
-                # Если фон картинки черный более чем n%, а текст белый, то инвертируем цвета
+                # If the background of the picture is more than n% black, and the text is white, then invert the colors
                 img_binary = cv2.bitwise_not(test_thresh) if percent_zeros < 40 else test_thresh
 
-                # отношение высоты изображения к шрифту и количеству строк
+                # We calculate the ratio of the height of the image to the font and the number of lines
                 scale_coef = font_size/new_font_size
                 max_dimension = max(img_binary.shape[0:2])
                 new_max_resolution = max_dimension/scale_coef
-                # Ресайзим избыточно большие изображения, ориентируясь на размер шрифта
+                # Resize excessively large images based on the desired font size
                 scaled_binary_img = self._resizeImage(img_binary, new_max_resolution)
                 binary_images.append(scaled_binary_img)
-        # возвращаем массив бинарных изображений, если они есть.
+        # return an array of binary images, if any.
         return binary_images if len(binary_images) > 0 else False
 
-    def _findBoxes(self, mask_img, quantity:int):
+    def _findBoxes(self, mask_img, quantity:int) -> list:
         """
-        Метод принимает бинарную маску-изображение, которая выделяет текстовые блоки.
-        Параметр quantity предназначен для фильтрования найденных боксов по размеру -
-        метод вернет боксы ограничивающие текст, с максимальной площадью в количестве равном quantity.
+        The method accepts a binary image mask that selects text blocks.
+        The quantity parameter is intended to limit the number of boxes, remove duplicates and very small boxes.
+        The method will return the maximum {quantity} of boxes with the maximum area.
         """
         contours, hierarchy = cv2.findContours(mask_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
         img_area = mask_img.shape[0]*mask_img.shape[1]
 
         for cont in contours:
-            # удаляем боксы площадь которых меньше порогового значения
+            # remove boxes whose area is less than the threshold value
             (x, y, w, h) = cv2.boundingRect(cont)
             if w*h < img_area/200:
                 continue
             else:
-                # Оставшиеся боксы расширяем(%) используя коэффициенты by, bx
+                # Expand the remaining boxes (%) using the coefficients by, bx so
+                # that the text is guaranteed to fit in them
                 by = int(h*0.02)
                 bx = int(w*0.05)
                 x1, x2 = abs(x - bx), (x + w + bx)
                 y1, y2 = abs(y - by), (y + h + bx)
-                # Проверяем не выходят ли координаты x2, y2 за пределы изображения
+                # We check if the coordinates x2, y2 go beyond the image and if they do,
+                # then the coordinates are equal to the maximum dimension of the image
                 if x2 > mask_img.shape[1]:
                     x2 = mask_img.shape[1]
                 if y2 > mask_img.shape[0]:
@@ -185,36 +182,33 @@ class ImageOCR:
 
                 new_W, new_H = (x2 - x1), (y2 - y1)
 
-                self.boxes.append([x1, y1, new_W, new_H])
-        self.boxes = self._findLargestBoxes(quantity=quantity) if len(self.boxes) > quantity else self.boxes
+                self._boxes.append([x1, y1, new_W, new_H])
+        self._boxes = self._findLargestBoxes(quantity=quantity) if len(self._boxes) > quantity else self._boxes
 
-    def _findLargestBoxes(self, quantity:int):
-        '''Метод принимает список всех найденных боксов и возвращает заданное
-        количество (не более чем :quantity:) боксов с наибольшей площадью'''
-        boxes_array = np.array(self.boxes)
+    def _findLargestBoxes(self, quantity:int) -> list:
+        '''The method takes a list of all found boxes and returns the given
+        number (not more than: quantity :) of boxes with the largest area'''
+        boxes_array = np.array(self._boxes)
         areas = np.prod(boxes_array[:,2:4], axis=1)
         max_areas_indises = np.argpartition(areas, -quantity)[-quantity:]
         bigest_boxes = [boxes_array[i].tolist() for i in max_areas_indises]
         return bigest_boxes
 
-    def _imageCropper(self, image, crop_coef:float):
-        """Метод обрезает картинку пропорционально кроп коэффициенту (числа с плавающей точкой от 0 до 1)
-        относительно центра изображения."""
+    def _imageCropper(self, image, crop_coef:float) -> np.ndarray:
+        """The method crops the image proportionally to the crop ratio (floating point numbers from 0 to 1)
+        relative to the center of the image."""
         x, y, h, w = 0, 0, image.shape[0], image.shape[1]
-        # Для корректного распознавания языка ширина дополнительно умножена на 1.5
+        # For correct language recognition, the width is additionally multiplied by 1.5 (you can experiment)
         new_w, new_h = (w*crop_coef*1.5), (h*crop_coef)
         new_x, new_y = (w - new_w), (h - new_h)
         cropped_img = image[int(new_y):int(new_y+new_h), int(new_x):int(new_x+new_w)]
         return cropped_img
 
-    def _recognizeText(self, conf:str, image):
-        """Text recognition method
-        1) Extract the region of interest from the image
-        2) Recognize image_to_text and seve results in the string.
-        """
+    def _recognizeText(self, conf:str, image) -> str:
+        """Text recognition method"""
         return pytesseract.image_to_string(image, config=conf)
 
-    def _detectLang(self, text:str):
+    def _detectLang(self, text:str) -> str:
         # remove all non-alphanumeric characters
         cleared_text = re.sub(r'[\W_0-9]+', ' ', text)
         DetectorFactory.seed = 0
@@ -224,7 +218,7 @@ class ImageOCR:
         alpha_3_lang_code = langdict.alpha_3
         return alpha_3_lang_code
 
-    def getText(self, text_lang:str, lang_detect:bool, crop:bool, set_font:int):
+    def getText(self, text_lang:str, lang_detect:bool, crop:bool, set_font:int) -> list:
         """If the language recognition flag is active, then we do a trial
         text recognition in the language passed to the method + English.
         We define exactly the language and repeat the refined, monolingual
@@ -244,38 +238,38 @@ class ImageOCR:
             binary_images = self._getBinaryImages(self.img, font_size, set_font)
         if binary_images == False:
             return False
-
+        # Loop through images prepared for OCR
         for image in binary_images:
-            #cv2.imshow('binary', image)
-            #cv2.waitKey(0)
             if lang_detect == True:
-
-                default_config = ('-l ' + text_lang + '+eng --oem 1 --psm 6')
-                # для ускорения распознавания языка исходное изображение обрезаем.
+                # If the image language is unknown, then the tesseract will
+                # make primary recognition in several languages, low quality
+                default_config = ('-l rus+deu+eng --oem 1 --psm 6')
+                # a cropped sample image is used to speed up language recognition.
                 sample_image = self._imageCropper(image, 0.6)
                 multilang_recog_text = self._recognizeText(default_config, sample_image)
                 # Detect language
                 recognized_lang = self._detectLang(multilang_recog_text)
-                custom_config = ('-l ' + recognized_lang + ' --oem 1 --psm 6')
+                # After the exact definition of the language, we make repeated
+                # recognition with the exact indication of the language
+                custom_config = (f'-l {recognized_lang} --oem 1 --psm 6')
                 text = self._recognizeText(custom_config, image)
                 self.text.append({recognized_lang:text})
             else:
-                config = ('-l ' + text_lang + ' --oem 1 --psm 6')
+                # Recognition option if the language of the text is known
+                config = (f'-l {text_lang} --oem 1 --psm 6')
                 text = self._recognizeText(config, image)
-
                 self.text.append({text_lang:text})
 
         return self.text
 
-    def drawBoxes(self, max_resolution):
+    def drawBoxes(self, max_resolution) -> np.ndarray:
         """The method of drawing of recognized texts and bounding rectangles of the image_to_text"""
-        # TODO можно задать индекс бокса в self.boxes в котором были найдены компоненты и отрисовывать только его
+        # TODO можно задать индекс бокса в self._boxes в котором были найдены компоненты и отрисовывать только его
         img_with_boxes = self.img
-        for box in self.boxes:
+        for box in self._boxes:
             (x,y,w,h) = box
             cv2.rectangle(img_with_boxes, (x, y), ((x + w), (y + h)), (255, 255, 0), 10)
         img_with_boxes = self._resizeImage(img_with_boxes, max_resolution)
-        #cv2.imshow('r', img_with_boxes)
-        #cv2.waitKey(0)
+
         return img_with_boxes
 
