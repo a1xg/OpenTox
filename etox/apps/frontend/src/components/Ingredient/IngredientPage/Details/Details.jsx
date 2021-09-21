25aeef7c944855162ac9b64a72d0428bc0f993ef
a1xg <64093496+a1xg@users.noreply.github.com>
2021-09-21 21:02:58 +0300

    Добавлена валидация данных перед отображением.

--- etox/apps/frontend/src/components/Ingredient/IngredientPage/Details/Details.jsx
+++ etox/apps/frontend/src/components/Ingredient/IngredientPage/Details/Details.jsx
@@ -3,25 +3,36 @@ import GridItem from '../../../GridItem/GridItem.jsx';
 import { Typography, List, ListItem, ListItemText } from "@material-ui/core";
 
 //TODO отрефакторить код: переписать на компонентах Material UI, возможно на компоненте Table
+
 const Details = (props) => {
     console.log('Details props', props);
 
     return (
         <div>
             <Typography variant='body1'>
-            CAS numbers {props.data.ingredient.cas_numbers.join(', ')}
+            CAS numbers {
+            props.data.ingredient.cas_numbers != null ? props.data.ingredient.cas_numbers.join(', ') : 'not data'
+            }
             </Typography>
             <Typography variant='body1'>
-            Colour Index number (CI) {props.data.ingredient.colour_index}
+            Colour Index number (CI) {
+            props.data.ingredient.colour_index != null ? props.data.ingredient.colour_index : 'not data'
+            }
             </Typography>
             <Typography variant='body1'>
-            EC numbers {props.data.ingredient.ec_numbers.join(', ')}
+            EC numbers {
+            props.data.ingredient.ec_numbers != null ? props.data.ingredient.ec_numbers.join(', ') : 'not data'
+            }
             </Typography>
             <Typography variant='body1'>
-            Functions {props.data.ingredient.functions.join(', ')}
+            Functions {
+            props.data.ingredient.functions != null ? props.data.ingredient.functions.join(', ') : 'not data'
+            }
             </Typography>
             <Typography variant='body1'>
-            PubChem page {props.data.ingredient.pubchem_cid}
+            PubChem page {
+            props.data.ingredient.pubchem_cid != null ?  props.data.ingredient.pubchem_cid : 'not data'
+            }
             </Typography>
             <Typography variant='body1'>
             Request statistics {props.data.ingredient.request_statistics} 
