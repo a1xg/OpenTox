5c54d21b00577f7f02b16e71c88ab3abb4cb473b
a1xg <64093496+a1xg@users.noreply.github.com>
2021-10-13 17:44:43 +0300

    The block of synonyms has been moved down, the grid hierarchy and styles have been corrected.

--- etox/apps/frontend/src/components/Ingredient/IngredientPage/IngredientPage.jsx
+++ etox/apps/frontend/src/components/Ingredient/IngredientPage/IngredientPage.jsx
@@ -12,11 +12,31 @@ import ItemCard from "../../ItemCard/ItemCard.jsx";
 import Breadcrumb from "./Breadcrumb/Breadcrumbs.jsx";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
+    root:{
+        alignItems: 'center',
+    },
+    topGrid: {
         flexGrow: 1,
         width: "100%",
         justifyContent: 'space-around',
+    },
+    col1: {
         alignItems: 'stretch',
+        justifyContent: 'space-around',
+        height:'100%'
+    },
+    col2: {
+        alignItems: 'stretch',
+        justifyContent: 'space-around',
+        height:'100%'
+    },
+    col2row:{
+        alignItems: 'center'
+    },
+    bottomGrid:{
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        //width:'100%'
     }
 }));
 
@@ -27,10 +47,12 @@ const IngredientPage = (props) => {
     return (
         <Container maxWidth={'lg'}>
             {props.backButton == true &&
-            <Breadcrumb namePage={props.searchResults.data.ingredient.main_name} />
+                <Breadcrumb namePage={props.searchResults.data.ingredient.main_name} />
             }
-            <Grid container direction="row" spacing={0} className={classes.root}>
-                <Grid item container xs={3} direction='column' spacing={2}>
+            <Grid container direction="column" spacing={2} className={classes.root}>
+
+            <Grid item xs={12} container direction="row" spacing={0} className={classes.topGrid} >
+                <Grid item container xs={4} direction='column' spacing={2} className={classes.col1}>
                     <Grid item>
                         <ItemCard title='Ingredient name'>
                             <Title mainName={props.searchResults.data.ingredient.main_name} />
@@ -46,11 +68,17 @@ const IngredientPage = (props) => {
                             <Details data={props.searchResults.data.ingredient} />
                         </ItemCard>
                     </Grid>
-
+                    {props.searchResults.data.ingredient.description != null &&
+                        <Grid item>
+                            <ItemCard title='Ingredient description'>
+                                <Description data={props.searchResults.data.ingredient.description} />
+                            </ItemCard>
+                        </Grid>
+                    }
                 </Grid>
 
-                <Grid item container xs={9} direction="column" spacing={2}>
-                    <Grid item container direction="row" spacing={2} >
+                <Grid item container xs={8} direction="column" spacing={2} className={classes.col2}>
+                    <Grid item container direction="row" spacing={2} className={classes.col2row}>
                         {props.searchResults.data.ingredient.hazard.ingredient_hazard_avg > 0 &&
                             <Grid item xs={6} >
                                 <ItemCard
@@ -76,31 +104,27 @@ const IngredientPage = (props) => {
                             </Grid>
                         }
                     </Grid>
-                    <Grid item>
+                    <Grid item >
                         <ItemCard title='Chart description'>
                             <Legend data={props.searchResults.data.ingredient.hazard} />
                         </ItemCard>
                     </Grid>
-                    {props.searchResults.data.ingredient.description != null &&
-                        <Grid item>
-                            <ItemCard title='Ingredient description'>
-                                <Description data={props.searchResults.data.ingredient.description} />
-                            </ItemCard>
-                        </Grid>
-                    }
-                    {props.searchResults.data.ingredient.synonyms != null &&
-                        props.searchResults.data.ingredient.synonyms.eng != null &&
-                        <Grid item>
+                </Grid>
+            </Grid>
+            {props.searchResults.data.ingredient.synonyms != null &&
+                    props.searchResults.data.ingredient.synonyms.eng != null &&
+                        <Grid item container xs={12} direction="row" spacing={2} className={classes.bottomGrid}>
+                            <Grid item xs={12}>
                             <ItemCard title='Synonyms'>
                                 <Synonyms
                                     data={props.searchResults.data.ingredient.synonyms.eng}
                                 />
                             </ItemCard>
-                        </Grid>
-                    }
-                </Grid>
+                            </Grid>
+                    </Grid>
+                }
             </Grid>
-        </Container>
+        </Container >
     )
 };
 
