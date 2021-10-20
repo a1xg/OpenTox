8baf87257e801fae4978bfa2282285dd8dc10427
a1xg <64093496+a1xg@users.noreply.github.com>
2021-10-20 14:08:44 +0300

    OCR config was changed

--- etox/apps/backend/ocr_settings.py
+++ etox/apps/backend/ocr_settings.py
@@ -5,5 +5,9 @@ FONT_SIZE = 40
 #Windows Tesseract path
 TESSERACT_PATH = 'D:/Program/Tesseract-OCR/tesseract.exe'
 
-# Linux Tesseract path
-#TESSERACT_PATH = '/app/.apt/usr/bin/tesseract'
+'''
+Для развертывания в Heroku не нужно конфигурировать pytesseract.tesseract_cmd и надо просто 
+добавить переменную среды:
+Key: tesseract 
+Value: ./.apt/usr/bin/tesseract
+'''
