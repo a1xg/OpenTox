4395d9a11d4bf12fae46d11b806a9f6a738e5e71
a1xg <64093496+a1xg@users.noreply.github.com>
2021-10-22 22:13:17 +0300

    Ingredient list pagination removed, error message styles corrected

--- etox/apps/frontend/src/components/SearchResults/ResultsPage/IngredientsList/IngredientsList.jsx
+++ etox/apps/frontend/src/components/SearchResults/ResultsPage/IngredientsList/IngredientsList.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { capitalize } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
@@ -10,34 +9,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import Container from '@material-ui/core/Container';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
 import Link from '@material-ui/core/Link';
 import IngredientRatingBar from '../../../Charts/IngredientRatingBar.jsx';
 import BriefStatistics from '../../../Charts/BriefStatistics.jsx';
-import TablePaginationActions from "./TablePaginationActions/TablePaginationActions.jsx";
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
-// TODO поправить ширину в мобильной версии, что бы не появлялся скроллбар
+ // TODO таблица уплывает за пределы контейнера в мобильной версии
 const IngredientsList = (props) => {
     const ingredients = props.data.product_ingredients;
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - ingredients.length) : 0;
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
 
     return (
         <Container>
@@ -50,10 +27,7 @@ const IngredientsList = (props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {(rowsPerPage > 0
-                        ? ingredients.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        : ingredients
-                    ).map((ingredient) => {
+                    {ingredients.map((ingredient) => {
                         return (
                             <TableRow key={ingredient.id}>
                                 <TableCell align="left">
@@ -82,32 +56,7 @@ const IngredientsList = (props) => {
                             </TableRow>
                         )
                     })}
-                    {emptyRows > 0 && (
-                        <TableRow >
-                            <TableCell colSpan={6} />
-                        </TableRow>
-                    )}
                 </TableBody>
-                <TableFooter>
-                    <TableRow >
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                            colSpan={3}
-                            count={ingredients.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            SelectProps={{
-                                inputProps: {
-                                    'aria-label': 'Ingredients per page',
-                                },
-                                native: true,
-                            }}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            ActionsComponent={TablePaginationActions}
-                        />
-                    </TableRow>
-                </TableFooter>
             </Table>
         </Container>
     )
