4b8771423013001665034641bd5dbf2088663a9b
a1xg <64093496+a1xg@users.noreply.github.com>
2021-10-26 20:37:29 +0300

    The gallery for the preview / image of the product has been improved.

--- etox/apps/frontend/src/components/SearchResults/ResultsPage/ProductPhoto/ProductPhoto.jsx
+++ etox/apps/frontend/src/components/SearchResults/ResultsPage/ProductPhoto/ProductPhoto.jsx
@@ -1,53 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core/styles';
 import Gallery from "react-grid-gallery";
 import Container from '@material-ui/core/Container';
-
-//Todo параметры thumbnailHeight thumbnailWidth необходимы и их надо вычислить
-
-const getPreviewSize = (imgSrc) => {
-    let img = new Image;
-    img.src = imgSrc;
-    img.onload = () => {
-        console.log('DIMENSIONS', img.width, img.height);
-    };
-
-};
-
-const useStyles = makeStyles((theme) => ({
-}));
+import { getPreviewSize } from '../../../tools.js'
+import useStyles from './styles.js';
 
 const ProductPhoto = (props) => {
-    const [image, setImage] = useState([{
-        src: "https://opentox.s3.eu-central-1.amazonaws.com/scan.png",
-        thumbnail: "https://opentox.s3.eu-central-1.amazonaws.com/scan.png",
-        caption: "Product image",
-        thumbnailWidth: 100,
-        thumbnailHeight: 100,
-    }]);
+    const classes = useStyles();
+    const [image, setImage] = useState();
 
     useEffect(() => {
-        const base64string = `data:image/jpeg;base64,${props.image.slice(2, -1)}`;
+        const newSize = getPreviewSize({
+            height: props.image.height,
+            width: props.image.width,
+            maxResolution: 300
+        });
+
+        const base64string = `data:image/jpeg;base64,${props.image.image}`;
         setImage([
             {
                 src: base64string,
                 thumbnail: base64string,
                 caption: "Product image",
-                thumbnailWidth: 100,
-                thumbnailHeight: 100,
+                thumbnailWidth: newSize.width,
+                thumbnailHeight: newSize.height,
             }
         ]);
-        getPreviewSize(base64string);
-    }, [props.image]);
+
+    }, [props.image.image]);
 
     return (
-        <Container styles={{ maxWidth: 100, maxHeigth: 200 }}>
-            <Gallery
-                images={image}
-                enableLightbox={true}
-                backdropClosesModal
-            />
+        <Container className={classes.container}>
+            {image != null &&
+                <Gallery
+                    images={image}
+                    enableLightbox={true}
+                    backdropClosesModal
+                />
+            }
         </Container>
     )
 };
---
+++ etox/apps/frontend/src/components/SearchResults/ResultsPage/ProductPhoto/styles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        position: 'relative',
+        justifyContent: 'center',
+        paddingRight:0,
+        paddingLeft:0,
+        width:'100%',
+    }
+}));
+
+export default useStyles;
